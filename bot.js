6ae0b1bcb8dacbd85a74b02b01d1817ea91b7876
MD2003 <53384251+MD2003@users.noreply.github.com>
2019-08-30 05:53:25 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,5 +1,5 @@
 const Discord = require('discord.js');
-const devs = ['237271695377956864'];
+const devs = ['389090790984515594'];
 const db = require('quick.db');
 const premium = ['470896018603376640']
 const client = new Discord.Client();   
